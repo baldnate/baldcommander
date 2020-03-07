591f9e724bd82610c01b6731043bcb7d1d84ba5f
baldnate <baldnate@gmail.com>
2020-03-07 13:01:41 -0600

    delinting

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ This is baldnate's automation app for wrangling stuff he needs to wrangle during
 - controlling his SWP123 A/V switch
 - changing scenes in OBS
 
-## Future Improvements 
+## Future Improvements
+
 - Adding the rest of my OBS workflow to this, as it would let me further shrink the OBS window while I am streaming
 - Cross fading between background music and game music (for raffles and BRB scenes)
 - Volume levels would be awesome, as that would let me minimize Voice Meeter Banana during streams
 - Switching Amarec modes (240p to 480i)
 - Anything else I find in my streaming workflow that isn't wholly integrated
-
 
 ## License
 
--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
@@ -30,7 +32,7 @@ class DVS304 {
 	}
 
 	build_url(command) {
-		return `${this.base_url}${this.dvs_escape(command)}`
+		return `${this.base_url}${this.dvs_escape(command)}`;
 	}
 
 	cmd(command) {
@@ -45,7 +47,7 @@ class DVS304 {
 	}
 
 	cmds(commandList) {
-		this.cmd(commandList.join(''))
+		this.cmd(commandList.join(''));
 	}
 
 	// cmds(commandList) {
@@ -66,32 +68,32 @@ document.addEventListener("keydown", function (e) {
 	}
 });
 
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('COM4', {baudRate: 9600})
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM4', {baudRate: 9600});
 
 
-const parser = port.pipe(new Readline({ delimiter: '\n' }))
-parser.on('data', console.log)
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+parser.on('data', console.log);
 port.on('error', function(err) {
-  console.log('Error: ', err.message)
-})
+  console.log('Error: ', err.message);
+});
 
 function sendCommand(command) {
 	port.write(command, function(err) {
-	  if (err) { return console.log('Error on write: ', err.message) }
-	})
+	  if (err) { return console.log('Error on write: ', err.message); }
+	});
 }
 
 function sendCommands(commandList) {
 	for (var i = 0; i < commandList.length; i++) {
-		sendCommand(commandList[i])
+		sendCommand(commandList[i]);
 	}
 }
 
-const OBSWebSocket = require('obs-websocket-js')
-const obs = new OBSWebSocket()
-obs.connect()
+const OBSWebSocket = require('obs-websocket-js');
+const obs = new OBSWebSocket();
+obs.connect();
 
 obs.on('error', err => {
 	console.error('socket error:', err);
@@ -99,66 +101,66 @@ obs.on('error', err => {
 
 obs.on('StreamStatus', data => {
 	console.log(data);
-})
+});
 
 obs.on('SwitchScenes', data => {
 	console.log(`New Active Scene: ${data.sceneName}`);
-})
+});
 
 function switchScene(sceneName) {
-	console.log('Switching scene to ', sceneName)
-	obs.send('SetCurrentScene', {'scene-name': sceneName})
+	console.log('Switching scene to ', sceneName);
+	obs.send('SetCurrentScene', {'scene-name': sceneName});
 }
 
 function registerScene(sceneData) {
-	document.querySelector(sceneData['button']).addEventListener(
+	document.querySelector(sceneData.button).addEventListener(
 		'click',
 		function() {
-			switchScene(sceneData['scene'])
+			switchScene(sceneData.scene);
 		}
-	)
+	);
 }
 
 function updateButton(button, state, trueStyle, falseStyle) {
 	if (state) {
-		document.getElementById(button).className = trueStyle
+		document.getElementById(button).className = trueStyle;
 	} else {
-		document.getElementById(button).className = falseStyle
+		document.getElementById(button).className = falseStyle;
 	}
 }
 
 function toggleMute(source, button, mode = null) {
 	if (mode == null) {
-		obs.send('ToggleMute', {'source': source})
+		obs.send('ToggleMute', {'source': source});
 	} else {
-		obs.send('SetMute' , {'source': source, 'mute': (mode == true)})
+		obs.send('SetMute' , {'source': source, 'mute': (mode == true)});
 	}
 	response = obs.send('GetMute', {'source': source}).then(response => {
-		console.log(response)
-		updateButton(button, response['muted'], "button pulsingRedBG", "button")
-	})
+		console.log(response);
+		updateButton(button, response.muted, "button pulsingRedBG", "button");
+	});
 }
 
 function toggleStartRecording(source, button) {
-	obs.send('StartStopRecording')
+	obs.send('StartStopRecording');
 	response = obs.send('GetMute', {'source': source}).then(response => {
-		console.log(response)
-		if (response['muted']) {
-			document.getElementById(button).className = "button pulsingRedBG"
+		console.log(response);
+		if (responsemuted) {
+			document.getElementById(button).className = "button pulsingRedBG";
 		} else {
-			document.getElementById(button).className = "button"
+			document.getElementById(button).className = "button";
 		}
-	})
+	});
 }
 
 scenes = [
 	{'scene': 'brb',   'button': '#sceneBRB'},
 	{'scene': 'Main',  'button': '#sceneMain'},
 	{'scene': 'outro', 'button': '#sceneWaffleIron'}
-]
+];
 
 for (var i = scenes.length - 1; i >= 0; i--) {
-	registerScene(scenes[i])
+	registerScene(scenes[i]);
 }
 
 function cvbs(port) {
@@ -228,28 +230,28 @@ switchData = [
 	// vcr - comp
 	{'button': '#vidsms',      'dvs304': ['9*0#'], 'swp123': [vga(4), cvbs(2)], 'crop': [50, 46, 46, 40]}
 
-]
+];
 
 function registerSwitchPort(portData) {
-	document.querySelector(portData['button']).addEventListener (
+	document.querySelector(portData.button).addEventListener (
 		'click',
 		function() {
 			//swp123_blank_all();
-			sendCommands(portData['swp123']);
+			sendCommands(portData.swp123);
 			sleep(500);
-			dvs304.cmds(portData['dvs304']);
+			dvs304.cmds(portData.dvs304);
 			obs.send('SetSceneItemProperties',
 				{
 					'scene-name': '4x3 frame',
 					'item': 'amarec_live',
 					'crop': {
-						'top': portData['crop'][0],
-						'bottom': portData['crop'][1],
-						'left': portData['crop'][2],
-						'right': portData['crop'][3]
+						'top': portDatacrop[0],
+						'bottom': portDatacrop[1],
+						'left': portDatacrop[2],
+						'right': portDatacrop[3]
 					}
 				}
-			)
+			);
 /*			response = obs.send('GetSceneItemProperties',
 				{
 					'scene-name': 'Main',
@@ -259,11 +261,11 @@ function registerSwitchPort(portData) {
 				console.log(response)
 			})
 */		}
-	)
+	);
 }
 
 for (var i = switchData.length - 1; i >= 0; i--) {
-	registerSwitchPort(switchData[i])
+	registerSwitchPort(switchData[i]);
 }
 
 document.querySelector('#vid-blank').addEventListener(
@@ -273,17 +275,17 @@ document.querySelector('#vid-blank').addEventListener(
 	}
 );
 
-nateMuted = false
+nateMuted = false;
 function muteNate() {
-	nateMuted = !nateMuted
-	toggleMute('VM B1 - Mics', 'nateMute', nateMuted)
+	nateMuted = !nateMuted;
+	toggleMute('VM B1 - Mics', 'nateMute', nateMuted);
 	obs.send('SetSceneItemProperties',
 		{
 			'scene-name':'Main',
 			'item': 'fancy cam',
 			'visible': !nateMuted
 		}
-	)
+	);
 	obs.send('SetSceneItemProperties',
 		{
 			'scene-name':'raffle time',
@@ -296,14 +298,14 @@ function muteNate() {
 document.querySelector('#nateMute').addEventListener (
 	'click',
 	function() {
-		muteNate()
+		muteNate();
 	}
 );
 
 document.querySelector('#switchMute').addEventListener (
 	'click',
 	function() {
-		sendCommand('[MUTE]')
+		sendCommand('[MUTE]');
 	}
 );
 
